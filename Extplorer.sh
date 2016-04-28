3b449ca9edce344ee1af75f1c700351251f5c57f
Mirko Dall'Armellina <mrwinch@tiscali.it>
2016-04-28 17:23:10 +0200

    Update Extplorer.sh

--- Extplorer.sh
+++ Extplorer.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 Download_File="https://extplorer.net/attachments/download/68/eXtplorer_2.1.9.zip"
 Extplorer_Version="2.1.9"
+Install_Dir="/usr/var/local/www/Extplorer"
 Confirm(){
   read -r -p "   Continue? [Y/n] " response
   case "$response" in
@@ -18,4 +19,9 @@ Confirm(){
 }
 /bin/echo "This script will install Extplorer ver. $Extplorer_Version"
 Confirm
-
+echo -e "Creating installation directory..."
+mkdir $Install_Dir
+chown -R www:www $Install_Dir
+echo -e "Downloading Extplorer..."
+fetch -o "Extplorer.zip" $Download_File
+tar xf "Extplorer.zip" -C $Install_Dir
