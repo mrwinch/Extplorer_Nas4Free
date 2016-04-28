b8235d1a5cbae1a9c1b452efadc51bdbb0a72515
Mirko Dall'Armellina <mrwinch@tiscali.it>
2016-04-28 17:11:27 +0200

    Update Extplorer.sh

--- Extplorer.sh
+++ Extplorer.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
-/bin/echo "Hello World!"
+Download_File="https://extplorer.net/attachments/download/68/eXtplorer_2.1.9.zip"
+Extplorer_Version="2.1.9"
+Confirm(){
+  read -r -p "   Continue? [Y/n] " response
+  case "$response" in
+      [yY][eE][sS]|[yY]) 
+                echo -e " Great! Moving on.."
+                 ;;
+      *)
+                # Otherwise exit...
+                echo " "
+                echo -e "Stopping script.."
+                echo " "
+                exit
+                ;;
+  esac  
+}
+/bin/echo "This script will install Extplorer ver. $Extplorer_Version"
+Confirm
+
