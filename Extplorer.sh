9edfd29ac03893ccc35461e6bd58b7a2b5b3572d
Mirko Dall'Armellina <mrwinch@tiscali.it>
2016-04-29 13:08:25 +0200

    Update Extplorer.sh

--- Extplorer.sh
+++ Extplorer.sh
@@ -4,6 +4,8 @@ Download_File="https://extplorer.net/attachments/download/68/eXtplorer_2.1.9.zip
 Extplorer_Version="2.1.9"
 Install_Dir="/usr/local/www/Extplorer"
 Extplorer_Add="https://raw.githubusercontent.com/mrwinch/Extplorer_Nas4Free/master/ExtPlorer.php"
+Extplorer_Dest="/usr/local/www/Extplorer.php"
+Extplorer_Cfg_Dest="/usr/local/www/Extplorer/config/.htusers.php"
 Extplorer_Cfg="https://raw.githubusercontent.com/mrwinch/Extplorer_Nas4Free/master/.htusers.php"
 Confirm(){
   read -r -p "   Continue? [Y/n] " response
@@ -53,7 +55,7 @@ pkg install -y php56-session
 echo -e "Installing php56-zlib (10/10)..."
 pkg install -y php56-zlib
 echo -e "Updating Web GUI..."
-fetch -o "usr/local/www/Extplorer.php" $Extplorer_Add
-chown www:www $Extplorer_Add
-rm "usr/local/www/Extplorer/config/.htusers.php"
-fetch -o "usr/local/www/Extplorer/config/.htusers.php" $Extplorer_Cfg
+fetch -o $Extplorer_Dest $Extplorer_Add
+chown www:www $Extplorer_Dest
+rm $Extplorer_Cfg_Dest
+fetch -o $Extplorer_Cfg_Dest $Extplorer_Cfg
