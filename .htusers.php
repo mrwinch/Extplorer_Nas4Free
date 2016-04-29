2fdd6025d4d33b079e9b142c94fe4d927f97fdfa
Mirko Dall'Armellina <mrwinch@tiscali.it>
2016-04-29 09:34:53 +0200

    Create .htusers.php

---
+++ .htusers.php
@@ -0,0 +1,7 @@
+<?php 
+	// ensure this file is being included by a parent file
+	if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
+	$GLOBALS["users"]=array(
+	array('admin','$2a$08$IJQWvN7KRijdhHAnVMfGouyIgEe3c86ZZuEQXY0pSEWLOfjaY16XW','/','http://localhost','1','','7',1),
+); 
+?>
