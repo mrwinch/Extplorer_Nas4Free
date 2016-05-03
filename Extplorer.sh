2bb012ad8109f0a77c507429938d9251b322b78a
Mirko Dall'Armellina <mrwinch@tiscali.it>
2016-05-03 17:42:50 +0200

    Update Extplorer.sh

--- Extplorer.sh
+++ Extplorer.sh
@@ -7,31 +7,29 @@ Extplorer_Add="https://raw.githubusercontent.com/mrwinch/Extplorer_Nas4Free/mast
 Extplorer_Dest="/usr/local/www/Extplorer.php"
 Extplorer_Cfg_Dest="/usr/local/www/Extplorer/config/.htusers.php"
 Extplorer_Cfg="https://raw.githubusercontent.com/mrwinch/Extplorer_Nas4Free/master/.htusers.php"
-FBegin_Target="\$menu['advanced']['menuitem'][] = array(\"desc\" => gettext(\"File Manager\"), \"link\" => \"/quixplorer/system_filemanager.php\", \"visible\" => TRUE);"
-FBegin_Dest="\$menu['advanced']['menuitem'][] = array(\"desc\" => gettext(\"Extplorer\"), \"link\" => \"/Extplorer.php\", \"visible\" => TRUE);/\n\$menu['advanced']['menuitem'][] = array(\"desc\" => gettext(\"File Manager\"), \"link\" => \"/quixplorer/system_filemanager.php\", \"visible\" => TRUE);"
-FBegin_File="/usr/local/www/fbegin.inc"
+GUI_Patch="https://raw.githubusercontent.com/mrwinch/Extplorer_Nas4Free/master/Gui_Patch.php"
 Rt='\033[m'
 Br='\033[1m'
 #************* Script's routines *********************************************
 Confirm(){
-  read -r -p "   Continue? [Y/n] ${Br}" response
+  read -r -p "   Continue? [Y/n] " response
   case "$response" in
       [yY][eE][sS]|[yY]) 
-                echo -e " ${Rt}Great! Moving on.."
+                echo -e " Great! Moving on.."
                  ;;
       *)
                 # Otherwise exit...
                 echo " "
-                echo -e "${Rt}Stopping script.."
+                echo -e "Stopping script.."
                 echo " "
                 exit
                 ;;
   esac  
 }
 #************* Let's start...
-echo -e "***********************"
+echo -e "${Br}***********************${Rt}"
 echo -e "   This script will install Extplorer ver. ${Br}$Extplorer_Version${Rt}"
-echo -e "***********************"
+echo -e "${Br}***********************${Rt}"
 Confirm
 echo -e "Creating installation directory..."
 mkdir $Install_Dir
@@ -43,32 +41,34 @@ echo -e "Extplorer downloaded and extracted..."
 echo -e "Removing Extplorer package..."
 rm Extplorer.zip
 echo -e "Now installing required packages..."
-echo -e "Installing php56 (1/10)..."
+echo -e "Installing php56 ${Br}(1/10)${Rt}..."
 pkg install -y php56
-echo -e "Installing php56-extensions (2/10)..."
+echo -e "Installing php56-extensions ${Br}(2/10)${Rt}..."
 pkg install -y php56-extensions
-echo -e "Installing php56-xmlrpc (3/10)..."
+echo -e "Installing php56-xmlrpc ${Br}(3/10)${Rt}..."
 pkg install -y php56-xmlrpc
-echo -e "Installing php56-gettext (4/10)..."
+echo -e "Installing php56-gettext ${Br}(4/10)${Rt}..."
 pkg install -y php56-gettext
-echo -e "Installing php56-mcrypt (5/10)..."
+echo -e "Installing php56-mcrypt ${Br}(5/10)${Rt}..."
 pkg install -y php56-mcrypt
-echo -e "Installing php56-mbstring (6/10)..."
+echo -e "Installing php56-mbstring ${Br}(6/10)${Rt}..."
 pkg install -y php56-mbstring
-echo -e "Installing php56-zip (7/10)..."
+echo -e "Installing php56-zip ${Br}(7/10)${Rt}..."
 pkg install -y php56-zip
-echo -e "Installing php56-gd (8/10)..."
+echo -e "Installing php56-gd ${Br}(8/10)${Rt}..."
 pkg install -y php56-gd
-echo -e "Installing php56-session (9/10)..."
+echo -e "Installing php56-session ${Br}(9/10)${Rt}..."
 pkg install -y php56-session
-echo -e "Installing php56-zlib (10/10)..."
+echo -e "Installing php56-zlib ${Br}(10/10)${Rt}..."
 pkg install -y php56-zlib
+echo -e "Updating Extplorer config..."
+rm $Extplorer_Cfg_Dest
+fetch -o $Extplorer_Cfg_Dest $Extplorer_Cfg
 echo -e "Updating Web GUI..."
 fetch -o $Extplorer_Dest $Extplorer_Add
 chown www:www $Extplorer_Dest
-rm $Extplorer_Cfg_Dest
-fetch -o $Extplorer_Cfg_Dest $Extplorer_Cfg
-sed "s/${FBegin_Target}/${FBegin_Dest}/g" $FBegin_File > "Mod.inc"
-rm $FBegin_File
-cp "Mod.inc" $FBegin_File
+echo -e "Patching Web GUI bar..."
+fetch -o "GUI_Patch.php" $GUI_Patch
+chmod a+x GUI_Patch.php
+./Gui_Patch.php
 echo -e "Installation complete: enjoy Extplorer $Extplorer_Version"
