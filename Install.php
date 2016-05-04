f6f74f0b070fe32e350de54a48bfe9aba7b464ba
Mirko Dall'Armellina <mrwinch@tiscali.it>
2016-05-04 13:12:44 +0200

    Update Install.php

--- Install.php
+++ Install.php
@@ -109,7 +109,7 @@ function File_Creator(){
 	var H=document.getElementById(\"header\");
 	var HN=document.getElementById(\"headernavbar\");	
 	var Fr=document.getElementById(\"frame\");		
-	Fr.style.height=(F.offsetTop-H.clientHeight-130-HN.clientHeight)+\"px\";
+	Fr.style.height=(F.offsetTop-H.clientHeight-150-HN.clientHeight)+\"px\";
 	}
 	</script>
 	<table width=\"100%\" border=\"0\ cellpadding=\"0\" cellspacing=\"0\" id=\"table\">
